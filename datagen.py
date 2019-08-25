f3ec468a5787cc323e23d0f8794f14ed498befd1
Sean Ang <seanang185@gmail.com>
2019-08-26 00:31:42 +0800

    changed to events v2 and refactored .run()

--- datagen.py
+++ datagen.py
@@ -1,87 +1,69 @@
 # Events v1 - flat dict of events
+# Events v2 - nested dict of events. Nesting accounts within. Changed to absolute time basis.
 
 events = [
     {
-        "event_type": "start"
+        "event_type": "expenses",
+        "event_time": 0,
+        "amount": 1000,
+        "delta_rate": {
+            "cash": -1
+        }
     },
     {
         "event_type": "new_job",
-        "time_delta": 6,
-        "bal_cash_shift": 0,
-        "bal_cash_rate": 3000
+        "event_time": 6,
+        "amount": 3000,
+        "delta_rate": {
+            "cash": 0.8,
+            "cpf": 0.2+0.18
+        }
     },
     {
-        "event_type": "cpf_adjustment",
-        "time_delta": 0,
-        "bal_cash_shift": 0,
-        "bal_cash_rate": -0.2*3000
+        "event_type": "family_allowance",
+        "event_time": 6,
+        "amount": 300,
+        "delta_rate": {
+            "cash": -1
+        }
     },
     {
-        "event_type": "cpf_adjustment",
-        "time_delta": 0,
-        "bal_cpf_rate": 0.2*3000
+        "event_type": "pay_raise",
+        "event_time": 6+12,
+        "amount": 500,
+        "delta_rate": {
+            "cash": 0.8,
+            "cpf": 0.2+0.18
+        }
     },
     {
-        "event_type": "expense_familyallowance",
-        "time_delta": 0,
-        "bal_cash_shift": 0,
-        "bal_cash_rate": -300
-    },
-    {
-        "event_type": "salary_increment",
-        "time_delta": 12,
-        "bal_cash_shift": 0,
-        "bal_cash_rate": 500
-    },
-    {
-        "event_type": "cpf_adjustment",
-        "time_delta": 0,
-        "bal_cash_shift": 0,
-        "bal_cash_rate": -0.2*500
-    },
-    {
-        "event_type": "cpf_adjustment",
-        "time_delta": 0,
-        "bal_cpf_rate": 0.2*500
-    },
-    {
-        "event_type": "expense_familyallowance",
-        "time_delta": 0,
-        "bal_cash_shift": 0,
-        "bal_cash_rate": -100
-    },
-    {
-        "event_type": "expense_necessities",
-        "time_delta": 0,
-        "bal_cash_shift": 0,
-        "bal_cash_rate": -1000
-    },
-    {
-        "event_type": "expense_luxury",
-        "time_delta": 0,
-        "bal_cash_shift": 0,
-        "bal_cash_rate": -100
-    },
-    {
-        "event_type": "insurance_purchase",
-        "time_delta": 6,
-        "bal_cash_shift": -2000,
-        "bal_cash_rate": -200,
-        "bal_insurance_shift": 2000,
-        "bal_insurance_rate": 200
+        "event_type": "insurance",
+        "event_time": 6+12,
+        "amount": 2000,
+        "delta_balance": {
+            "cash": -1,
+            "insurance": 1            
+        },
+        "delta_rate": {
+            "cash": -0.1,
+            "insurance": 0.1
+        }
     },
     {
         "event_type": "bto",
-        "time_delta": 2*12,
-        "bal_cpf_shift": -20000
+        "event_time": 6+12+2*12,
+        "amount": 20000,
+        "delta_balance": {
+            "cash": -1
+        }
     },
     {
         "event_type": "marriage",
-        "time_delta": 2*12,
-        "bal_cash_shift": -40000
-    },
-    {
-        "event_type": "end"
+        "event_time": 6+12+2*12+2*12,
+        "amount": 40000,
+        "delta_balance": {
+            "cash": -1
+        }
     }
 ]
 
--- sim.py
+++ sim.py
@@ -10,27 +10,19 @@ START = 23 * 12       ## 24 years into life
 END = 30 * 12         ## retirement age
 
 # TODO :: Think about concepts of acceleration / velocity / dollar
-
+ACCOUNTS = ['cash', 'cpf', 'insurance', 'total_earned']
 
 class Simulator():
-    balances = {
-        "cash": 0,
-        "cpf": 0,
-        "insurance": 0
-    }
-    rates = {
-        "cash": 0,
-        "cpf": 0,
-        "insurance": 0
-    }
+    balances = { acc:0 for acc in ACCOUNTS }
+    rates = { acc:0 for acc in ACCOUNTS }
     time_current = 0
     result = []
 
     def __init__(self, START, END):
-        self.time_current = START
-        self.end_month = END
+        # self.time_current = START
+        self.end_month = END - START
 
-    def increment(self,):
+    def increment(self):
         for acc, rate in self.rates.items():
             self.balances[acc] += rate
         self.time_current += 1
@@ -43,40 +35,44 @@ class Simulator():
         self.result.extend(states)
 
     def run(self, events):
+        # TODO :: vectorize this run
+        # initialize 
+        newstate = copy.deepcopy(self.balances)
+        newstate['month'] = self.time_current
+        self.result.append(newstate)
         for event in events:
-            if event["event_type"] == "start":
-                newstate = copy.deepcopy(self.balances)
-                newstate['month'] = self.time_current
-                self.result.append(newstate)
-            elif event["event_type"] == "end":
-                # wrap up simulation by extrapolating till the end
-                duration = self.end_month - self.time_current
-                self.advance(duration)
-            else:
-                # is there really a need to handle each special type?
+            # advance in time to the present
+            duration = event["event_time"] - self.time_current
+            self.advance(duration)
 
-                # advance in time to the present
-                duration = event["time_delta"]
-                self.advance(duration)
+            # alter the present state of rates
+            if 'delta_rate' in event:
+                for acc, mult in event['delta_rate'].items():
+                    actual_amount = event['amount'] * mult
+                    self.rates[acc] += actual_amount
+                    if mult > 0:
+                        self.rates['total_earned'] += actual_amount
 
-                # alter the present state of balances
-                self.balances["cash"] += event.get("bal_cash_shift", 0)
-                self.balances["cpf"] += event.get("bal_cpf_shift", 0)
-                self.balances["insurance"] += event.get("bal_insurance_shift", 0)
+            # alter the present state of balances
+            if 'delta_balance' in event:
+                for acc, mult in event['delta_balance'].items():
+                    actual_amount = event['amount'] * mult
+                    self.balances[acc] += actual_amount
+                    if mult > 0:
+                        self.balances['total_earned'] += actual_amount
 
-                # alter the present state of rates
-                self.rates["cash"] += event.get("bal_cash_rate", 0)
-                self.rates["cpf"] += event.get("bal_cpf_rate", 0)
-                self.rates["insurance"] += event.get("bal_insurance_rate", 0)
+        # finish calculating the remaining time
+        self.advance(self.end_month - self.time_current)
 
-# Actually run this
-scenario = Simulator(START, END)
-scenario.run(events)
-df = pd.DataFrame(scenario.result)
+if __name__ == '__main__':
+    # Actually run this
+    scenario = Simulator(START, END)
+    scenario.run(events)
+    df = pd.DataFrame(scenario.result)
 
-df['year'] = df['month'] / 12
-df.drop('month', axis=1, inplace=True)
-df.set_index('year', inplace=True)
-df.plot()
+    df['year'] = (START+df['month']) / 12
+    df.drop('month', axis=1, inplace=True)
+    df.set_index('year', inplace=True)
+    df.plot()
 
-plt.show()
+    plt.show()
